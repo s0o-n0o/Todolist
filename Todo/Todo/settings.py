e78058e000803925acdf6b36bc654d8d521a90dd
s0o-n0o <sh8576278@gmail.com>
2022-08-29 19:18:42 +0900

    make login_forms

--- Todo/Todo/settings.py
+++ Todo/Todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-v7(b7(ni=!1e%@u*$%r6&=l88-!rasg&=a2n(cusj)u))3d436
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [127.0.0.1]
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- Todo/Todo/urls.py
+++ Todo/Todo/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('TodoApp/',include("TodoApp.urls"))
+    path('TodoApp/',include("TodoApp.urls")),
+    path('user/',include("user.urls")),
 ]
